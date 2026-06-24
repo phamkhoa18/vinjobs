396f05edc4271e964027e340f8f6bddfdf378a84
phamkhoa18 <phamkhoa3092003@gmail.com>
2026-06-24 22:56:17 +0700

    Remove test files before submission

--- backend-vinjobs/test-expired.js
+++
@@ -1,4 +0,0 @@
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
-const token = jwt.sign({ id: '60d21b4667d0d8992e610c85' }, process.env.JWT_SECRET || 'vinjobs-secret-key-2026', { expiresIn: '-1s' });
-console.log(token);
